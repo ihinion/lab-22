2442181b06195486b908fa649122a299b80eeffe
ilia <jamiashvili@gmail.com>
2020-10-26 00:24:00 +0600

    Добавлен ViewS=set для цитат и подключен в urls, поправлены сериализаторы для создания и обновления цитат

--- source/api/urls.py
+++ source/api/urls.py
@@ -1,9 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import QuoteViewset
+
+router = DefaultRouter()
+router.register('quote', QuoteViewset)
 
 
 app_name = 'api'
 
 
 urlpatterns = [
-
+    path('', include(router.urls))
 ]
